2fed335bdf04221fbc3b5f40c157b48e80d628a2
Ivan <mad.genius69@gmail.com>
2023-11-16 14:08:55 +0200

    16/11

---
+++ lessons of JS/scripts/script.js
@@ -0,0 +1,66 @@
+// dz1
+// let name;
+// let city;
+// name = 'Ivan';
+// city= name;
+// console.log(city)
+
+// dz 2
+
+// let name = "Olga";
+// console.log(`привіт ${1}`);
+// console.log(`привіт ${"name"}`);
+// console.log(`привіт ${name}`);
+
+// привіт 1
+// script.js:12 привіт name
+// script.js:13 привіт Olga
+
+// dz 3
+
+// let a = "5";
+// let b = "13cvb";
+// let c = "12.9sxdcfgv";
+
+// console.log(Number.parseInt(a));
+// console.log(Number.parseInt(b));
+// console.log(Number.parseInt(c));
+
+// dz 5
+// console.log(Math.max(20, 10, 50, 40));
+
+// dz 6
+
+// console.log(Math.random()*(4-2)+2);
+
+// dz 7
+// const message = "Welcome to Bahamas!";
+
+// console.log(message.length);
+
+// dz 8
+// let mes = "messege";
+// console.log(mes.toLocaleUpperCase());
+
+// dz 9
+
+// let obj = {};
+// obj.name = "ivan";
+// obj.age = "26";
+// obj.city = "Lviv";
+// console.log(obj);
+
+// delete obj.city;
+
+// console.log(obj);
+
+// obj["like flowers"] = true;
+
+// console.log(obj);
+
+// // dz 10
+
+// for (key in obj) {
+//   console.log(obj[key]);
+//   console.log(key);
+// }
