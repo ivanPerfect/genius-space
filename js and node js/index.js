fd36c00aa00e83ecff0a328d7837286629b1efae
Ivan <mad.genius69@gmail.com>
2023-11-20 15:02:31 +0200

    20/11

--- js and node js/index.js
+++ js and node js/index.js
@@ -1,3 +1,27 @@
-import { name } from "./module";
+// import { name } from "./module.js";
+// import "./style.css";
+// import isEmail from "validator/lib/isEmail";
+// import isURL from "validator/lib/isURL";
+// import toDate from "validator/lib/toDate";
 
-console.log("your name " + name);
+// console.log("your name " + name);
+
+// var validator = require("validator");
+
+// console.log(validator.isEmail("foo@bar.com")); //=> true)
+
+// //2 способ
+
+// console.log(isEmail("foo@bar.com")); //=> true)
+
+// console.log(isURL("https://www.npmjs.com/package/validator"));
+
+// console.log(toDate("https://www.npmjs.com/package/validator"));
+
+// console.log(toDate("12/1/1222"));
+
+var moment = require('moment');
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+console.log(moment().format("MMM Do YY"));
--- js and node js/module.js
+++ js and node js/module.js
@@ -1,1 +1,1 @@
-const name = "ivan";
+export const name = "ivan";
