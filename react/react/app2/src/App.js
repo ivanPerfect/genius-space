9349ddac24bac0e94de298c36cdac6864a1de11c
Ivan <mad.genius69@gmail.com>
2023-11-23 15:45:41 +0200

    23/11

--- react/react/app2/src/App.js
+++ react/react/app2/src/App.js
@@ -1,24 +1,11 @@
-import {useEffect, useLayoutEffect, useState} from 'react';
+import { useEffect, useState, useCallback } from "react";
+import UncontlorForm from "./compo/UncontlorForm";
 
 function App() {
-const[value,setValue]= useState(0);
-  
-  useEffect(() => {
-    console.log('Chenged')
-  }, [value]);
 
-  const handleClick=()=>{
-    setValue(value+1);
-  }
-  useLayoutEffect(()=>{
-    console.log("useLayoutEffect")
-  },[handleClick]);
-
-  
   return (
-    <div className="App">
-      <h1>{value}</h1>
-     <button onClick={handleClick}>TOUCH ME</button>
+    <div>
+      <UncontlorForm/>
     </div>
   );
 }
